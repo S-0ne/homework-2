0799159bdfbcdf36df7951d05df9686e63ccaa14
S-0ne <144223532+S-0ne@users.noreply.github.com>
2023-09-12 13:13:25 +0300

    Add files via upload

---
+++ let u = 1.js
@@ -0,0 +1,15 @@
+let u = 1
+let k = "ukd"
+let r = NaN
+let a = true
+let i = "undefined"
+
+let ne = {
+    name: "S_0ne",
+    surname: "Sasha",
+    age: 17,
+    home: "Stanislav"
+}
+console.log(u, k, r, a, i)
+console.table(ne)
+
